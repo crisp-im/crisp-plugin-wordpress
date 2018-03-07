4a18f86e0081a7e10d87c2067bf72e3ecedeec0e
crisp.im <crisp.im@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-03-07 07:40:05 +0000

    Add changelog
    git-svn-id: http://plugins.svn.wordpress.org/crisp/trunk@1835107 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ crisp.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * @package Crisp
+ * @version 0.21
+ * Plugin Name: Crisp
+ * Plugin URI: http://wordpress.org/plugins/crisp/
+ * Description: Crisp is a Livechat plugin
+ * Author: Crisp IM
+ * Version: 0.21
+ * Author URI: https://crisp.chat
+ *
+ * Text Domain: crisp
+ * Domain Path: /languages/
+*/
+
+add_action('admin_menu', 'crisp_create_menu');
+
+function crisp_create_menu() {
+  add_menu_page(__('Crisp Settings', 'crisp'), __('Crisp Settings', 'crisp'), 'administrator', __FILE__, 'crisp_plugin_settings_page' , 'https://crisp.chat/favicon.png');
+  add_action('admin_init', 'register_crisp_plugin_settings' );
+  add_action('admin_init', 'register_crisp_plugin_onboarding');
+}
+
+function register_crisp_plugin_onboarding() {
+  $onboarding = get_option('crisp_onboarding');
+  $website_id = get_option('website_id');
+
+  if (empty($website_id) && (empty($onboarding) || !$onboarding)) {
+    update_option("crisp_onboarding", true);
+    wp_redirect(admin_url('admin.php?page='.plugin_basename(__FILE__)));
+  }
+}
+
+function register_crisp_plugin_settings() {
+  register_setting( 'crisp-plugin-settings-group', 'website_id' );
+  add_option('crisp_onboarding', false);
+}
+
+function crisp_plugin_settings_page() {
+  if (isset($_GET["crisp_website_id"]) && !empty($_GET["crisp_website_id"])) {
+    update_option("website_id", $_GET["crisp_website_id"]);
+  }
+
+  if (isset($_GET["crisp_verify"]) && !empty($_GET["crisp_verify"])) {
+    update_option("website_verify", $_GET["crisp_verify"]);
+  }
+
+  $website_id = get_option('website_id');
+  $is_crisp_working = isset($website_id) && !empty($website_id);
+  $http_callback = "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  $add_to_crisp_link = "https://app.crisp.chat/initiate/plugin/aca0046c-356c-428f-8eeb-063014c6a278?payload=$http_callback";
+?>
+
+<link rel="stylesheet" href="<?php echo plugins_url("assets/style.css", __FILE__ );?>">
+  <?php
+  if ($is_crisp_working) {
+  ?>
+
+  <div class="wrap crisp-wrap">
+    <div class="crisp-modal">
+      <h2 class="crisp-title"><?php _e('Connected with Crisp.', 'crisp'); ?></h2>
+      <p class="crisp-subtitle"><?php _e('You can now use Crisp from your homepage.', 'crisp'); ?></p>
+      <a class="crisp-button crisp-neutral" href="https://app.crisp.chat/settings/website/<?php echo $website_id ?>"><?php _e('Go to my Crisp settings', 'crisp'); ?></a>
+
+      <a class="crisp-button crisp" href="https://app.crisp.chat/website/<?php echo $website_id ?>/inbox/"><?php _e('Go to my Inbox', 'crisp'); ?></a>
+
+      <a class="crisp-button crisp-neutral" href="<?php echo $add_to_crisp_link; ?>"><?php _e('Reconfigure', 'crisp'); ?></a>
+
+      
+    </div>
+
+    <p class="crisp-notice"><?php _e('Loving Crisp <b style="color:red">♥</b> ? Rate us on the <a target="_blank" href="https://wordpress.org/support/plugin/crisp/reviews/?filter=5">Wordpress Plugin Directory</a>', 'crisp'); ?></p>
+  </div>
+
+  <?php
+  } else {
+  ?>
+  <div class="wrap crisp-wrap">
+    <div class="crisp-modal">
+      <h2 class="crisp-title"><?php _e('Connect with Crisp.', 'crisp'); ?></h2>
+      <p class="crisp-subtitle"><?php _e('This link will redirect you to Crisp and configure your Wordpress. Magic', 'crisp'); ?></p>
+      <a class="crisp-button crisp" href="<?php echo $add_to_crisp_link; ?>"><?php _e('Connect with Crisp', 'crisp'); ?></a>
+    </div>
+  </div>
+  <?php
+  }
+}
+
+add_action('wp_head', 'crisp_hook_head', 1);
+
+function crisp_sync_wordpress_user() {
+  $output = "";
+
+
+  if (is_user_logged_in()) {
+    $current_user = wp_get_current_user();
+  }
+
+  if (!isset($current_user)) {
+    return "";
+  }
+
+  $website_verify = get_option('website_verify');
+
+  $email = $current_user->user_email;
+  $nickname = $current_user->display_name;
+
+  if (!empty($email) && empty($website_verify)) {
+    $output .= '$crisp.push(["set", "user:email", "' . $email . '"]);';
+  } else if (!empty($email)) {
+    $hmac = hash_hmac("sha256", $email, $website_verify);
+    $output .= '$crisp.push(["set", "user:email", ["' . $email . '", "' . $hmac . '"]]);';
+  }
+
+  if (!empty($nickname)) {
+    $output .= '$crisp.push(["set", "user:nickname", "' . $nickname . '"]);';
+  }
+
+  return $output;
+}
+
+function crisp_sync_woocommerce_customer() {
+  $output = "";
+  if (!class_exists("WooCommerce") || is_admin()) {
+    return $output;
+  }
+
+  $customer = WC()->session->get("customer");
+
+  if ($customer == NULL) {
+    return $output;
+  }
+
+  if (isset($customer["phone"]) && !empty($customer["phone"])) {
+    $output .= '$crisp.push(["set", "user:phone", "' . $customer["phone"] . '"]);';
+  }
+
+  $nickname = "";
+
+  if (isset($customer["first_name"]) && !empty($customer["first_name"])) {
+    $nickname = $customer["first_name"];
+  }
+  if (isset($customer["last_name"]) && !empty($customer["last_name"])) {
+    $nickname .= " ".$customer["last_name"];
+  }
+
+  if (!empty($nickname)) {
+    $output .= '$crisp.push(["set", "user:nickname", "' . $nickname . '"]);';
+  }
+
+  $data = array();
+  $data_keys = array(
+    "company",
+    "address",
+    "address_1",
+    "address_2",
+    "postcode",
+    "state",
+    "country",
+    "shipping_company",
+    "shipping_address",
+    "shipping_address_1",
+    "shipping_address_2",
+    "shipping_state",
+    "shipping_country",
+  );
+
+  foreach ($data_keys as $key) {
+    if (isset($customer[$key]) && !empty($customer[$key])) {
+      $data[] = '["'. $key . '", "' . $customer[$key] . '"]';
+    }
+  }
+
+  if (count($data) > 0) {
+    $output .= '$crisp.push(["set", "session:data", [[' . implode(",", $data) . ']]]);';
+  }
+
+  return $output;
+}
+
+function crisp_hook_head() {
+  $website_id = get_option('website_id');
+  $locale = str_replace("_", "-", strtolower(get_locale()));
+
+  if (!in_array($locale, array("pt-br", "pt-pr"))) {
+    $locale = substr($locale, 0, 2);
+  }
+
+  if (!isset($website_id) || empty($website_id)) {
+    return;
+  }
+
+  $output="<script data-cfasync='false'>
+    window.\$crisp=[];
+    CRISP_RUNTIME_CONFIG = {
+      locale : '$locale'
+    };
+    CRISP_WEBSITE_ID = '$website_id';";
+
+
+  $output .= "(function(){
+      d=document;s=d.createElement('script');
+      s.src='https://client.crisp.chat/l.js';
+      s.async=1;d.getElementsByTagName('head')[0].appendChild(s);
+    })();";
+
+  $output .= crisp_sync_wordpress_user();
+  $output .= crisp_sync_woocommerce_customer();
+
+  $output .= "</script>";
+  
+  echo $output;
+}
---
+++ readme.txt
@@ -0,0 +1,135 @@
+=== Crisp Live Chat ===
+Contributors: crisp.chat
+Donate link: https://crisp.chat
+Tags: livechat, chat, live chat, crisp, free-live-chat, live-chat, live support, chat-plugin, woocommerce
+Requires at least: 3.0.1
+Tested up to: 4.8
+Stable tag: 4.7
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+Boost your visitor satisfaction with Crisp, the best live chat software. Free Wordpress Plugin. Use it on your mobile or on your computer.
+
+== Description ==
+
+Crisp is a free and beautiful live chat platform for interacting with customers. Use Crisp to improve your customer satisfaction.
+
+[youtube https://www.youtube.com/watch?v=pGCrBRtytQQ]
+
+The best way to chat with your customers.
+
+Crisp allows you to interact with your customers the way you want to. From sales to customer support, Crisp keeps your workflow simple. Add teammates to support chat and email, all from a single user interface.
+
+See what your visitors are typing.
+
+Not all visitors to your website are digital natives. 
+For many, it takes time to type a response. With Crisp, you can see what your visitors are typing in real time, allowing you to respond with the right information in little to no time.
+
+Available on mobile and desktop
+
+Crisp is available on iOS, Android, WIndows, and Mac OS operating systems.
+
+You can use it in your browser too. It's up to you.
+
+Affordable customer support.
+
+Crisp takes a different business approach than other live chat services. We do not sell a limited chatbox service—with Crisp, the base chat service is free and unlimited, more than enough to cover the basic needs of anyone using the service.
+
+Use paid plugins to extend the power of your Crisp features.
+
+Working hours
+
+Set your working hours in the Crisp dashboard to prevent receiving chat requests during lunchtime, or your special date.
+
+See who your visitors are
+
+With Crisp, you can see your visitor's important information, such as email, full name, picture, location, social networks, and browsed webpages.
+
+Be notified
+
+ * Offline? Crisp can send notifications to all your mobile devices.
+ * Not replying? Crisp will forward your chat to your mailbox. From there, you can directly reply by email—Crisp will sync everything up.
+
+Integrate with other apps
+
+You can integrate Crisp with Slack, your e-mails, Messenger, Twitter, Telegram and SMS.
+
+Cool chat experience
+
+From smileys to animations, our chat system is fresh, cool, and simple to use. Just try it! 
+
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/crisp` directory, or install the plugin through the WordPress plugins screen directly.
+2. Use the Settings->Crisp screen and click on "Link with Crisp"
+
+== Screenshots ==
+
+1. Crisp dashboard
+2. Mobile app
+
+== Changelog ==
+
+= 0.21 =
+* Auto-set language
+
+= 0.20 =
+* Verify emails using HMAC
+
+= 0.19 =
+* Woocommerce compatibility
+
+= 0.18 =
+* Migrates .IM to .CHAT
+
+= 0.17 =
+* This release is fixing a bug
+
+= 0.16 =
+* This release is fixing a bug
+
+= 0.15 =
+* Simplifies the Crisp setup process
+
+= 0.14 =
+* Design improvement
+
+= 0.13 =
+* Auto-restore chats
+
+= 0.12 =
+* This version improves asynchronous API
+
+= 0.11 =
+* This version improves asynchronous API
+
+= 0.10 =
+* Design improvements
+
+= 0.9 =
+* This version fixes conflicts with Cloudflare
+
+= 0.8 =
+* Username is now automatically synced
+
+= 0.7 =
+* Design improvements
+
+= 0.6 =
+* Handles asynchronous API calls
+
+= 0.5 =
+* New way to install Crisp
+
+= 0.4 =
+* Bump up
+
+= 0.3 =
+* New JS API
+
+= 0.2 =
+* Add e-mail if setted
+
+= 0.1 =
+* Crisp first release
